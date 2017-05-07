49fcb9e72f668c9608b402febf4efcdb563515db
sawra <sawra@uw.edu>
2017-05-07 16:05:35 -0700

    removed Admin Theme, added nav bar component

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -5,9 +5,5 @@ import Header from './Header'
 export default (props)=>(
   <div>
     <Header />
-    <Link to='/dashboard'>Dashboard</Link>
-    <Link to='/talktodb'>Talk To Database</Link>
-    <Link to='/explorer'>Explorer</Link>
-    <Link to='/home'>Home</Link>
     {props.children}
   </div>)
