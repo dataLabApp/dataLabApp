a297b317590d7233068ce41bad320229444670c9
Andrew Hookom <hookom@gmail.com>
2017-05-08 13:52:00 -0400

    added a text edit box to explorer with save and revert buttons

--- app/components/ExplorerView.jsx
+++ app/components/ExplorerView.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import SQLForm from './SQLForm.jsx'
 import WidgetCard from './WidgetCard.jsx'
+import D3TextEditor from './D3TextEditor.jsx'
 
 export default (props)=>(
   <div>Welcome to the explorer!
     <SQLForm />
     <WidgetCard />
+    <D3TextEditor />
   </div>)
