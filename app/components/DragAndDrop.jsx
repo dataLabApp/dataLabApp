3000c582b7f7a4d94b59997aa4e435f51dbbd09d
sawra <sawra@uw.edu>
2017-05-09 10:24:24 -0700

    new drag&drop starting to work

--- app/components/DragAndDrop.jsx
+++ app/components/DragAndDrop.jsx
@@ -31,21 +31,3 @@ class DragAndDrop extends Component {
 
 export default DragAndDrop;
 
-
-// var MyFirstGrid = React.createClass({
-//   render: function() {
-//     // layout is an array of objects, see the demo for more complete usage
-//     var layout = [
-//       {i: 'a', x: 0, y: 0, w: 1, h: 2, static: true},
-//       {i: 'b', x: 1, y: 0, w: 3, h: 2, minW: 2, maxW: 4},
-//       {i: 'c', x: 4, y: 0, w: 1, h: 2}
-//     ];
-//     return (
-//       <ReactGridLayout className="layout" layout={layout} cols={12} rowHeight={30} width={1200}>
-//         <div key={'a'}>a</div>
-//         <div key={'b'}>b</div>
-//         <div key={'c'}>c</div>
-//       </ReactGridLayout>
-//     )
-//   }
-// });
