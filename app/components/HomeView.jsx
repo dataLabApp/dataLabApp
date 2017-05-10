91eeb0e3f0d42fe41e232bb80ae352b68ed71ea8
sawra <sawra@uw.edu>
2017-05-10 15:33:19 -0700

    dashboards list out on home page

--- app/components/HomeView.jsx
+++ app/components/HomeView.jsx
@@ -1,6 +1,35 @@
-import React from 'react'
+import React, { Component } from 'react';
+import { connect } from 'react-redux'
 
-    // <h1>Welcome Home</h1>
-export default (props)=> (
-  <div>Welcome Home</div>
-)
+class Home extends Component {
+
+  constructor(props){
+    super(props);
+    this.state= {dashboards: props.dashboards};
+  }
+  
+  render(){
+    return (
+      <div className="container">
+          { this.state.dashboards &&
+            this.state.dashboards.map((dashboard, i)=><div key={i}>{dashboard.title}</div>)
+          }
+      </div>
+    );
+  }
+
+}
+
+
+
+// ----------------------- Container -----------------------
+
+const mapStateToProps = (state, ownProps) => {
+  return {
+      dashboards: state.dashboards.dashboards
+  }
+}
+
+const mapDispatchToProps = null
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
