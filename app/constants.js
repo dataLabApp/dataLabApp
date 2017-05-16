880cb7e9dcce2899c8cc2f7fbfc79803e30fe088
Andrew Hookom <hookom@gmail.com>
2017-05-15 23:58:38 -0400

    charts beginning to respond to user inputs

--- app/constants.js
+++ app/constants.js
@@ -8,6 +8,8 @@ export const ItemTypes = {
 
 export const DEFAULT_TEMPLATE = fs.readFileSync(path.join(path.dirname(window.location.pathname),'/app/utils/barChartTemplate.js'),{encoding: 'utf-8'})
 
+export const HEADLESS_TEMPLATE = fs.readFileSync(path.join(path.dirname(window.location.pathname),'/app/utils/headlessBarChartTemplate.js'),{encoding: 'utf-8'})
+
 export const ROOT_PATH = path.dirname(window.location.pathname)
 
 export const snapSensitivity = 10
--- app/utils/chartGenerators.js
+++ app/utils/chartGenerators.js
@@ -1,22 +1,29 @@
 
-let d3 = require('d3')
-let fs = require('fs')
-let path = require('path')
+const d3 = require('d3')
+const fs = require('fs')
+const path = require('path')
 import {DEFAULT_TEMPLATE} from '../constants'
 import ReactFauxDom from 'react-faux-dom'
 
-export function chartGenerator(userGeneratedCode = DEFAULT_TEMPLATE){
+export function chartGenerator(userGeneratedCode = DEFAULT_TEMPLATE) {
   eval(userGeneratedCode)
   return window.explorerFauxNode.toReact()
 }
 
-// export function createChartGenerator(config){
-//   return function(userGeneratedCode){
-//     config
-//   }
-// }
+function parseConfig(config) {
+  window.TEMPDB = config.data
+  return `let data = JSON.parse('${JSON.stringify(config.data)}')
+  const x = '${config.x.dataColumn}'
+  const y = '${config.y.dataColumn}'
+  const chartTitle = '${config.title}'`
+}
 
-export function storeChartGenerator(userGeneratedCode){
+export function customChartGenerator(config, template) {
+  const parsedConfig = parseConfig(config)
+  return chartGenerator(parsedConfig + template)
+}
+
+export function storeChartGenerator(userGeneratedCode) {
   return () => chartGenerator(userGeneratedCode)
 }
 
