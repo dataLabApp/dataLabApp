23af70119c3b9c12fae2e728a1b4fb677dcb5f1d
Bruce Grugett <bgrugett@gmail.com>
2017-05-16 17:11:46 -0400

    reload chart on state change

--- app/components/DragAndDrop.jsx
+++ app/components/DragAndDrop.jsx
@@ -1,60 +1,63 @@
-const ReactGridLayout = require('react-grid-layout');
-import React, { Component } from 'react';
+const ReactGridLayout = require('react-grid-layout')
+import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import Chart from './Chart'
 import DashboardCard from './DashboardCard'
-import styles from  '../../node_modules/react-grid-layout/css/styles.css'
+import styles from '../../node_modules/react-grid-layout/css/styles.css'
 import styles2 from '../../node_modules/react-resizable/css/styles.css'
 import {updateDashboardLayout} from '../reducers/dashboardReducer'
 
-
 class DragAndDrop extends Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            title: props.dashboards.currentDashboard.title,
-            cards: props.dashboards.currentDashboard.cards
-        }
-        this.onLayoutChange = this.onLayoutChange.bind(this);
-        this.updateDashboardLayout = this.props.updateDashboardLayout.bind(this); 
+  constructor(props) {
+    super(props)
+    this.state = {
+      title: props.dashboards.currentDashboard.title,
+      cards: props.dashboards.currentDashboard.cards
     }
-    print(){
-        window.print();
-    }
-    onLayoutChange(layout) {
-        let newLayout = this.props.dashboards.currentDashboard.cards.map((card,i)=> Object.assign(card, layout[i]));
-        this.setState({
-            cards: newLayout
-        })
-    }
+    this.onLayoutChange = this.onLayoutChange.bind(this)
+    this.updateDashboardLayout = this.props.updateDashboardLayout.bind(this)
+  }
+  print() {
+    window.print()
+  }
+  onLayoutChange(layout) {
+    const newLayout = this.props.dashboards.currentDashboard.cards.map((card, i) => Object.assign(card, layout[i]))
+    this.setState({
+      cards: newLayout
+    })
+  }
 
-    componentWillUnmount(){
-        this.updateDashboardLayout(this.state.title, this.state.cards);
-    }
+  componentWillReceiveProps(nextProps) {
+    this.setState({title: nextProps.dashboards.currentDashboard.title,
+      cards: nextProps.dashboards.currentDashboard.cards})
+  }
 
-    render(){
+  componentWillUnmount() {
+    this.updateDashboardLayout(this.state.title, this.state.cards)
+  }
 
+  render() {
         //  var layout = [
         //     {i: 'a', x: 0, y: 0, w: 2, h: 2},
         //     {i: 'b', x: 1, y: 0, w: 3, h: 2, minW: 2},
         //     {i: 'c', x: 4, y: 0, w: 1, h: 2},
         //     {i: "hi", x: 0, y: Infinity, w: 2, h:2}
         //     ];
-            var divStyle = { 
-                borderStyle: 'solid',
-                borderWidth: '0.5px'
-            }
+    var divStyle = {
+      borderStyle: 'solid',
+      borderWidth: '0.5px'
+    }
 
-        return(<div>
-            <ReactGridLayout className="layout" layout={this.state.cards} cols={12} rowHeight={30} width={1200} ref="rgl"  onLayoutChange={this.onLayoutChange}>
+    return (<div>
+            <ReactGridLayout className="layout" layout={this.state.cards} cols={12} rowHeight={30} width={1200} ref="rgl" onLayoutChange={this.onLayoutChange}>
 
                 {
-                    this.state.cards.map((card)=>(<div key={card.i} style={divStyle}><DashboardCard card ={card} /></div>))
+                    this.state.cards.map((card) => (<div key={card.i} style={divStyle}><DashboardCard card ={card} /></div>))
                 }
             </ReactGridLayout>
             </div>
-        );
-    }
+    )
+  }
 }
                 // <div key={'b'} style = {divStyle}>b</div>
                 // <div key = {'c'} style = {divStyle}><DashboardCard /></div>
@@ -63,17 +66,15 @@ class DragAndDrop extends Component {
 //
 // ----------------------- Container -----------------------
 
-const mapStateToProps = (state, ownProps) => {
-  return {
-      dashboards: state.dashboards,
-      currentDashboard: state.currentDashboard
-  }
-}
-
-const mapDispatchToProps = (dispatch) =>({
-    updateDashboardLayout: (dashboardTitle, layout) => {
-        dispatch(updateDashboardLayout(dashboardTitle, layout));
-    }
+const mapStateToProps = (state, ownProps) => ({
+  dashboards: state.dashboards,
+  currentDashboard: state.currentDashboard
 })
 
-export default connect(mapStateToProps, mapDispatchToProps)(DragAndDrop);
+const mapDispatchToProps = (dispatch) => ({
+  updateDashboardLayout: (dashboardTitle, layout) => {
+    dispatch(updateDashboardLayout(dashboardTitle, layout))
+  }
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(DragAndDrop)
