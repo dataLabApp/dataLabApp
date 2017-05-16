fd90414b9012dea40882564d5a600d883b6c0c78
Andrew Hookom <hookom@gmail.com>
2017-05-16 15:19:41 -0400

    not sure

--- app/utils/headlessBarChartTemplate.js
+++ app/utils/headlessBarChartTemplate.js
@@ -4,8 +4,8 @@ const yAxisLabel = 'Dollars'
 let margin = {top: 20, right: 5, bottom: 50, left: 50}
 // here, we want the full chart to be 700x200, so we determine
 // the width and height by subtracting the margins from those values
-let fullWidth = 700
-let fullHeight = 200
+let fullWidth = 1000
+let fullHeight = 1000
 // the width and height values will be used in the ranges of our scales
 let width = fullWidth - margin.right - margin.left
 let height = fullHeight - margin.top - margin.bottom
@@ -21,8 +21,9 @@ let svg = window.d3.select(window.explorerFauxNode)
 // translate it to leave room for the left and top margins
 .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
 
+
 const xLabels = data.map(function(i) {
-  return i[x]
+return i[x]
 })
 
 // x value determined by month
