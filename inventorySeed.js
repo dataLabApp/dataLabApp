b2b4ce134350a3038a14b900241234cb60342023
Bruce Grugett <bgrugett@gmail.com>
2017-05-20 17:54:05 -0400

    inventory chart

--- inventorySeed.js
+++ inventorySeed.js
@@ -1,26 +1,25 @@
 
 const pg = require('pg')
-const client = new pg.Client('postgres://localhost/headsetLaunch')
+const client = new pg.Client('postgres://localhost/headSetLaunch')
 const path = require('path')
 
-const seedQuery= `INSERT _________________PUT YOUR ROWS HERE__________________`
+const seedQuery=`INSERT INTO "inventory" (Location,Inventory) VALUES ('Seattle',416),('Ithaca',604),('Atlanta',596),('Miami',393);`
+client.connect()
 
-    client.connect()
+client.query(`DROP TABLE IF EXISTS inventory`, function(err, data) {
+  if (err)console.error(err)
+  else console.log('dropped inventory')
+})
 
-    client.query(`DROP TABLE IF EXISTS inventory`, function(err, data){
-      if(err)console.error(err)
-      else console.log('dropped inventory')
-    })
-    client.query(`CREATE TABLE inventory _________put your scheme from generator here_________`, function(err, data){
-      if(err)console.error(err)
-      else{
-        console.log('table created')
-      }
-    })
-    client.query(seedQuery, function(err, data){
-      if(err)console.log(err)
-      else{
-        console.log('looks like seeding inventory worked')
-        client.end()
-      }
-    })
+client.query(`CREATE TABLE inventory (id SERIAL PRIMARY KEY, Location varchar(255), Inventory integer NULL)`, function(err, data) {
+  if (err) console.error(err)
+  else console.log('table created')
+})
+
+client.query(seedQuery, function(err, data) {
+  if (err) console.log(err)
+  else {
+    console.log('looks like seeding inventory worked')
+    client.end()
+  }
+})
