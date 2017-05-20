fce42fc9c277ebc763e35150712cff53e7ec3962
sawra <sawra@uw.edu>
2017-05-19 19:40:22 -0700

    trying to implement dashboard change using selector tool

--- app/components/DashboardView.jsx
+++ app/components/DashboardView.jsx
@@ -4,6 +4,7 @@ import DragAndDrop from './DragAndDrop'
 import {connect} from 'react-redux'
 import {setCurrentDashboard} from '../reducers/dashboardReducer'
 var PrintTemplate = require('react-print')
+import {Form, FormGroup, Button, ControlLabel, FormControl, ListGroup, ListGroupItem} from 'react-bootstrap'
 
 // export default class DashboardView extends Component {
 const DashboardView = (props) => {
@@ -14,16 +15,25 @@ const DashboardView = (props) => {
   return (
     <div className = "container-fluid">
         <button onClick={print} className="pull-right" id="react-no-print"><span className="glyphicon glyphicon-download"></span></button>
-        <ul style={{listStyle: 'none'}}>
-          <h3>Choose Dashboard</h3>
-          { props.dashBoards.dashboards.map(db => (
-            <li key={db.id} ><a onClick= { () => props.setCurrentDashboard(db.id)}>{db.title}</a> </li>
-        )) }
-        </ul>
-            <div className="container">
+        <div className="row">
+        <div className="col-sm-3">
+
+        <FormGroup controlId="formControlsSelect">
+              <ControlLabel>Select a Dashboard</ControlLabel>
+              <FormControl componentClass="select" placeholder="select" onChange= {props.setCurrentDashboard}>
+              {
+              props.dashBoards.dashboards.map((db)=>{
+                return <option key = {db.id} value={db.id}>{db.title}</option>
+              })
+              }
+              </FormControl>
+            </FormGroup>
+        </div>
+        <div className="col-sm-9">
                 <DragAndDrop />
-            </div>
+        </div>
     </div>
+  </div>
   )
 }
 
@@ -35,7 +45,9 @@ const mapStateToProps = (state) => (
   }
 )
 const mapDispatchToProps = (dispatch) => ({
-  setCurrentDashboard: (dbId) => {
+  setCurrentDashboard: (event) => {
+    event.preventDefault()
+    let dbId = event.target.value
     dispatch(setCurrentDashboard(dbId))
   }
 })
